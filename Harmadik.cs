b610813a0262627cf3a9d324f17e151dfe1a22c8
Nguyen Van Tamás <nguyen.van.tamas@petrik.hu>
2026-01-29 13:47:16 +0100

    Harmadik osztály kész

---
+++ Harmadik.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teszt1
+{
+	internal class Harmadik
+	{
+		Dictionary<Adat, int> stat;
+		public Harmadik()
+		{
+			stat = new Dictionary<Adat, int>();
+		}
+	}
+}

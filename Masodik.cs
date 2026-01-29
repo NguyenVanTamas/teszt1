521cac6f5b369630c5b61ae54e38db4d1d479353
Nguyen Van Tamás <nguyen.van.tamas@petrik.hu>
2026-01-29 13:33:38 +0100

    Második osztály kész

---
+++ Masodik.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teszt1
+{
+	internal class Masodik
+	{
+		List<Adat> list;
+
+		public Masodik()
+		{
+			list = new List<Adat>();
+		}
+	}
+}
